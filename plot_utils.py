ba1040471ebeb6a29518616f2281af9a597cebcd
yunnaidan <yunnaidan@pku.edu.cn>
2020-01-05 20:41:17 +0800

    add plot function for DVI result

--- plot_utils.py
+++ plot_utils.py
@@ -1,3 +1,5 @@
+import os
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -42,3 +44,59 @@ def toy_results_plot(data, data_generator, hypers=None, predictions=None):
     plt.ylabel('y')
     plt.ylim([-3,2])
     plt.legend()
+
+def UCI_result_plot(
+        dataset_name,
+        summary_path,
+        times,
+        axes,
+        train=False,
+        test=True,
+        b_epoch=0,
+        e_epoch=100,
+        shape='logprob'):
+
+    length = e_epoch - b_epoch
+    train_lst = []
+    test_lst = []
+    for time in range(times):
+        summary_file = os.path.join(summary_path, str(time), 'summaries.json')
+        with open(summary_file, 'r') as f:
+            summary = json.load(f)
+        train_log = []
+        test_log = []
+        for itera in summary[b_epoch:e_epoch]:
+            train_log.append(itera['train'][0][0][shape])
+            test_log.append(itera['test'][0][0][shape])
+        train_lst.append(train_log)
+        test_lst.append(test_log)
+    train_mean = np.mean(np.array(train_lst), axis=0)
+    test_mean = np.mean(np.array(test_lst), axis=0)
+    train_std = np.std(np.array(train_lst), axis=0)
+    test_std = np.std(np.array(test_lst), axis=0)
+
+    epoches = np.arange(b_epoch, length + b_epoch, 1)
+    if train:
+        axes.plot(epoches, train_mean, label='train')
+        axes.fill_between(
+            epoches,
+            train_mean -
+            0.5 *
+            train_std,
+            train_mean +
+            0.5 *
+            train_std, alpha=0.2)
+    if test:
+        axes.plot(epoches, test_mean, label='test')
+        axes.fill_between(
+            epoches,
+            test_mean -
+            0.5 *
+            test_std,
+            test_mean +
+            0.5 *
+            test_std, alpha=0.2)
+
+    axes.set_title(dataset_name)
+
+    return test_mean, test_std
